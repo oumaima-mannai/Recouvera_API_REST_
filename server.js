8dd30fe38d4e77ab35bdf3d34d7523d5ab4653fb
oumaima-mannai <manaoumaima28@gmail.com>
2026-03-12 23:56:40 +0100

    setup mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const app = require('./app');
+const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
+        await connectDB();
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
