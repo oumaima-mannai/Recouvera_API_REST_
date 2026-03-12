06b78ab7ca15f4ba360dc98fabc87bfc4c233b77
oumaima-mannai <manaoumaima28@gmail.com>
2026-03-12 23:53:09 +0100

    init project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Routes Placeholder
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Recouvra+ API' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+module.exports = app;
